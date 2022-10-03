e4010645434319356c874473de97a2f07e7716dd
ADIRA\10091062 <raja.sitompul@adira.co.id>
2022-10-03 10:23:46 +0700

    Init

--- main.go
+++ main.go
@@ -8,12 +8,11 @@ import (
 
 func main() {
 	r := gin.Default()
-	port:=os.Getenv("PORT")
 	// routers.ApiRouter(r)
 	routers.AuthRouter(r)
 	// routers.MasterRouter(r)
 	// routers.AdministratorRouter(r)
-	
+
 	port := os.Getenv("PORT")
 	r.Run(":"+port)
 }
