4b929200aba34dbefdde05830a936349c08e5c5c
ADIRA\10091062 <raja.sitompul@adira.co.id>
2022-10-09 02:12:21 +0700

    Init

--- controllers/user_c.go
+++ controllers/user_c.go
@@ -3,9 +3,6 @@ import (
 	"github.com/gin-gonic/gin"	
 	"wms/models"
 )
-func GetUser(c *gin.Context)(models.User){
-	// rst:=models.Query()
-	models.StoreData()
-	var u models.User
-	return u
+func GetUser(c *gin.Context)([] models.User){
+	return models.Find(map[string]interface{}{"username":"salomo07"})
 }
--- models/user_m.go
+++ models/user_m.go
@@ -1,30 +1,78 @@
 package models
 import (
+	kivik "github.com/go-kivik/kivik/v3"
+	"strconv"
 	"log"
 	"time"
 	"context"
 	"wms/config"
 )
 var (db=config.Connect())
-
-func Query()(User){
-	selector:=map[string]interface{}{"username":"salomo07"}
-	rows,errq := db.Query(context.TODO(),"_design/user", "_view/user",selector)
-
-	log.Println(rows,errq,"ddd")
-	var u User
-	// err := rows.ScanDoc(&u)
-	// if err != nil {
-	//     panic(err)
-	// }
-	return u
+func Find(s map[string]interface{})([] User){
+	sss:=map[string]interface{}{"selector":s}
+	rows,err := db.Find(context.TODO(),sss,map[string]interface{}{"include_docs":true})
+	if err != nil {
+	    panic(err)
+	}
+	var result [] User
+	for rows.Next() {
+	    var u User
+	    if err := rows.ScanDoc(&u); err != nil {
+			panic(err)
+		}
+		
+		result=append(result, u)
+	    /* do something with doc */
+	}
+	log.Println(result,"ddd")
+	return result
 }
+func Query()(){
+	rows, err := db.Query(context.TODO(), "_design/user", "_view/bar", kivik.Options{
+		"startkey": `"foo"`,                           // Quotes are necessary so the
+		"endkey":   `"foo` + kivik.EndKeySuffix + `"`, // key is a valid JSON object
+	})
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		var doc interface{}
+		if err := rows.ScanDoc(&doc); err != nil {
+			panic(err)
+		}
+		/* do something with doc */
+	}
+	if rows.Err() != nil {
+		panic(rows.Err())
+	}
+		// return rows
+	}
+// func Query()(){
+// 	rows,err := db.Query(context.TODO(),"_design/user", "_view/user",kivik.Options{"include_docs": true})
+
+// 	if err != nil {
+// 	    panic(err)
+// 	}
+// 	for rows.Next() {
+// 	    var u User
+// 	    if err := rows.ScanDoc(&u); err != nil {
+// 	        panic(err)
+// 	    }
+// 	    log.Println(u,"ddd")
+// 	    /* do something with doc */
+// 	}
+// 	if rows.Err() != nil {
+// 	    panic(rows.Err())
+// 	}
+// 	// log.Println(rows)
+// 	// return rows
+// }
 
 func StoreData(){
 	timenow:=time.Now().Unix()
 	u := User{Username: "salomo07", Password: "fff"}
 	log.Println(timenow)
-	_, err := db.Put(context.TODO(),"kodok", u)
+	_, err := db.Put(context.TODO(),strconv.FormatInt(timenow, 10), u)
 	if err != nil {
 	    panic(err)
 	}
@@ -32,4 +80,26 @@ func StoreData(){
 type User struct {
     Username      string `json:"username"`
     Password     string    `json:"password"`
+    Contact     Contact    `json:"contact"`
 }
+type Selector struct {
+    Selector      User `json:"selector"`
+    Limit		int `json:"limit"`
+}
+type Contact struct {
+    Mobile      string `json:"mobile"`
+    Email		string `json:"email"`
+}
+// type Selector struct {
+//     Selector      User `json:"selector"`
+//     Limit		int `json:"limit"`
+//     Skip int  `json:"skip"`
+//     Short []Short `json:"skip"`
+//     Field []string `json:"fields"`
+// }
+// type Short struct {
+
+// }
+// type Field struct {
+	
+// }
