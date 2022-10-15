b97a15c450e5bd40dbc36d9913708a759ea51c3a
salomo08 <v.raja.sitompul@adira.co.id>
2022-10-16 00:40:27 +0700

    Try install Redis

--- config/redis.go
+++ config/redis.go
@@ -1,13 +1,26 @@
 package config
 
 import(
-	_"github.com/go-redis/redis/v9"
+	"context"
+	"os"
+	"github.com/joho/godotenv"
+	"github.com/go-redis/redis/v9"
 )
-
+var ctx = context.Background()
+func init() {
+  connect()
+}
 func connect (){
-	// rdb := redis.NewClient(&redis.Options{
-	// 	Addr:	  "localhost:6379",
-	// 	Password: "", // no password set
-	// 	DB:		  0,  // use default DB
-	// })
+	er := godotenv.Load()
+	if er !=nil{
+		panic("Fail to load .env file")
+	}
+	REDIS_HOST:=os.Getenv("REDIS_HOST")
+	REDIS_PASS:=os.Getenv("REDIS_PASS")
+	REDIS_PORT:=os.Getenv("REDIS_PORT")
+	opt, _ := redis.ParseURL("rediss://:"+REDIS_PASS+"@"+REDIS_HOST+":"+REDIS_PORT)
+  	client := redis.NewClient(opt)
+  	client.Set(ctx, "foo", "bar", 0)
+	val := client.Get(ctx, "foo").Val()
+	print(val)
 }
