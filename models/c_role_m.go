366d2dfae99b861e595005522307892fa8d9476b
ADIRA\10091062 <raja.sitompul@adira.co.id>
2022-11-17 17:34:10 +0700

    Definising Model

--- models/c_role_m.go
+++ models/c_role_m.go
@@ -1,12 +1,11 @@
 package models
 
 type RoleModel struct {
-	Id       string     `json:"_id"`
-	Rev      string     `json:"_rev"`
-	Name     string     `json:"name" binding:"required"`
-	Codename string     `json:"codename" binding:"required"`
-	Menus    MenuModels `json:"menus"`
-	Role     string     `json:"role"`
-	Table    string     `json:"table"`
-	Desc     string     `json:"desc"`
+	Id       string `json:"_id"`
+	Rev      string `json:"_rev"`
+	Name     string `json:"name" binding:"required"`
+	Codename string `json:"codename" binding:"required"`
+	Role     string `json:"role"`
+	Table    string `json:"table"`
+	Desc     string `json:"desc"`
 }
