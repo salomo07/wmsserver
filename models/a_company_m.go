fca9fabe073c55c09e9eb9f05caf0458a22cbfc3
ADIRA\10091062 <raja.sitompul@adira.co.id>
2022-11-23 17:31:42 +0700

    model

--- models/a_company_m.go
+++ models/a_company_m.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"encoding/json"
+	"wms/config"
+)
+
 type CompanyModels struct {
 	Id      string         `json:"_id"`
 	Rev     string         `json:"_rev"`
@@ -15,4 +20,13 @@ type CompanyModels struct {
 type ContactCompany struct {
 	Email  string `json:"email"`
 	Mobile string `json:"mobile" binding:"required"`
+}
+
+func FindCompanyByR(jsonData []byte) string {
+	companyData := config.FindDoc("mastercompany", string(jsonData))
+	var comObject CompanyModels
+	json.Unmarshal([]byte(companyData), &comObject)
+}
+func FindCompanyByC(basiccred string, jsonData []byte) string {
+	return config.FindDocByCompany(basiccred, "mastercompany", string(jsonData))
 }
